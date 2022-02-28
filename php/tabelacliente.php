bc64826cefc53e5505bcefd1669237f8666d92cf
Voigt0 <voigtrodrigo0@gmail.com>
2022-02-27 23:21:25 -0300

    Update tabelacliente.php

--- php/tabelacliente.php
+++ php/tabelacliente.php
@@ -51,7 +51,6 @@
                     <th scope="col">Telefone</th>
                     <th scope="col">CPF</th>
                     <th scope="col">Email</th>
-                    <th scope="col">Senha</th>
                     <th scope="row">DETALHE</th>
                     <th scope="row">ALTERAR</th>
                     <th scope="row">EXCLUIR</th>
@@ -86,8 +85,7 @@
                     <td scope="row"><?php echo $linha['CLI_TELEFONE'];?></td>
                     <td scope="row"><?php echo $linha['CLI_CPF'];?></td>
                     <td scope="row"><?php echo $linha['CLI_EMAIL'];?></td>
-                    <td scope="row"><?php echo $linha['CLI_SENHA'];?></td>
-                    <td scope="row"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/></svg></td>
+                    <td scope="row"><a href="detalhecliente.php?id=<?php echo $linha['CLI_ID'];?>"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/></svg></a></td>
                     <td scope="row"><a href="cadcliente.php?id=<?php echo $linha['CLI_ID'];?>&comando=update"><img src="../img/history-solid.svg" style="width: 2rem;"></a></td>
                     <td><a onclick="return confirm('Deseja mesmo excluir?')" href="acao.php?id=<?php echo $linha['CLI_ID'];?>&tabela=cliente&comando=deletar"><img src="../img/trash.svg" style="width: 2rem;"></a></td>
                 </tr>
