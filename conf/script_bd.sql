98585cdd3b174073d6aba716add7451d3e1fce4b
Voigt0 <voigtrodrigo0@gmail.com>
2022-03-03 19:25:53 -0300

    Update script_bd.sql

--- conf/script_bd.sql
+++ conf/script_bd.sql
@@ -15,6 +15,22 @@ CREATE SCHEMA IF NOT EXISTS `biblioteca` DEFAULT CHARACTER SET utf8 ;
 USE `biblioteca` ;
 
 -- -----------------------------------------------------
+-- Table `biblioteca`.`cliente`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `biblioteca`.`cliente` (
+  `CLI_ID` INT NOT NULL AUTO_INCREMENT,
+  `CLI_NOME` VARCHAR(45) NULL,
+  `CLI_SOBRENOME` VARCHAR(45) NULL,
+  `CLI_NASCIMENTO` DATE NULL,
+  `CLI_TELEFONE` VARCHAR(45) NULL,
+  `CLI_CPF` VARCHAR(45) NULL,
+  `CLI_EMAIL` VARCHAR(45) NULL,
+  `CLI_SENHA` VARCHAR(45) NULL,
+  PRIMARY KEY (`CLI_ID`))
+ENGINE = InnoDB;
+
+
+-- -----------------------------------------------------
 -- Table `biblioteca`.`endereco`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `biblioteca`.`endereco` (
@@ -23,26 +39,11 @@ CREATE TABLE IF NOT EXISTS `biblioteca`.`endereco` (
   `END_CIDADE` VARCHAR(45) NULL,
   `END_RUA` VARCHAR(45) NULL,
   `END_NUMERO` INT NULL,
-  PRIMARY KEY (`END_ID`))
-ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `biblioteca`.`cliente`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `biblioteca`.`cliente` (
-  `CLI_ID` INT NOT NULL AUTO_INCREMENT,
-  ` CLI_NOME` VARCHAR(45) NULL,
-  `CLI_SOBRENOME` VARCHAR(45) NULL,
-  `CLI_NASCIMENTO` DATE NULL,
-  `CLI_TELEFONE` VARCHAR(45) NULL,
-  `CLI_CPF` VARCHAR(45) NULL,
-  `CLI_EMAIL` VARCHAR(45) NULL,
-  `END_ID` INT NOT NULL,
-  PRIMARY KEY (`CLI_ID`),
-  CONSTRAINT `fk_cliente_endereco`
-    FOREIGN KEY (`END_ID`)
-    REFERENCES `biblioteca`.`endereco` (`END_ID`)
+  `cliente_CLI_ID` INT NOT NULL,
+  PRIMARY KEY (`END_ID`),
+  CONSTRAINT `fk_endereco_cliente1`
+    FOREIGN KEY (`cliente_CLI_ID`)
+    REFERENCES `biblioteca`.`cliente` (`CLI_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
@@ -54,7 +55,7 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS `biblioteca`.`editora` (
   `EDI_ID` INT NOT NULL AUTO_INCREMENT,
   `EDI_NOME` VARCHAR(45) NULL,
-  `EDI_FUNDACAO` YEAR(4) NULL,
+  `EDI_FUNDACAO` DATE NULL,
   `EDI_FUNDADOR` VARCHAR(45) NULL,
   `EDI_SEDE` VARCHAR(45) NULL,
   PRIMARY KEY (`EDI_ID`))
@@ -70,7 +71,7 @@ CREATE TABLE IF NOT EXISTS `biblioteca`.`titulo` (
   `TIT_AUTOR` VARCHAR(45) NULL,
   `TIT_NUMEROPAG` INT NULL,
   `TIT_IDIOMA` VARCHAR(45) NULL,
-  `TIT_LANCAMENTO` YEAR(4) NULL,
+  `TIT_LANCAMENTO` DATE NULL,
   `EDI_ID` INT NOT NULL,
   PRIMARY KEY (`TIT_ID`),
   CONSTRAINT `fk_titulo_editora1`
@@ -130,20 +131,20 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `biblioteca`.`tit_gen`
+-- Table `biblioteca`.`gen_exe`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `biblioteca`.`tit_gen` (
-  `titulo_TIT_ID` INT NOT NULL,
-  `genero_GEN_ID` INT NOT NULL,
-  PRIMARY KEY (`titulo_TIT_ID`, `genero_GEN_ID`),
-  CONSTRAINT `fk_titulo_has_genero_titulo1`
-    FOREIGN KEY (`titulo_TIT_ID`)
-    REFERENCES `biblioteca`.`titulo` (`TIT_ID`)
+CREATE TABLE IF NOT EXISTS `biblioteca`.`gen_exe` (
+  `GEN_ID` INT NOT NULL,
+  `EXE_ID` INT NOT NULL,
+  PRIMARY KEY (`GEN_ID`, `EXE_ID`),
+  CONSTRAINT `fk_genero_has_exemplar_genero1`
+    FOREIGN KEY (`GEN_ID`)
+    REFERENCES `biblioteca`.`genero` (`GEN_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT `fk_titulo_has_genero_genero1`
-    FOREIGN KEY (`genero_GEN_ID`)
-    REFERENCES `biblioteca`.`genero` (`GEN_ID`)
+  CONSTRAINT `fk_genero_has_exemplar_exemplar1`
+    FOREIGN KEY (`EXE_ID`)
+    REFERENCES `biblioteca`.`exemplar` (`EXE_ID`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
