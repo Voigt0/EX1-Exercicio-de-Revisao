526917ba4d2256db5a37cebfbe01d2ff1e2ff2f3
Voigt0 <voigtrodrigo0@gmail.com>
2022-03-08 08:03:51 -0300

    Update menu.php

--- php/menu.php
+++ php/menu.php
@@ -1,5 +1,5 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<style>body {background-color: #DFD3C3;font-weight: bolder;}</style>
+<style>body {background-image: url("../img/images.jpg");font-weight: bolder;}</style>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
   <a class="navbar-brand" href="../index.php">Menu</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
