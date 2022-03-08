28329aaa1333eef1ab191f3ea35d1166d82671bc
Voigt0 <voigtrodrigo0@gmail.com>
2022-03-08 10:53:40 -0300

    Update menu.php

--- php/menu.php
+++ php/menu.php
@@ -1,7 +1,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-<style>body {background-image: url("../img/images.jpg");font-weight: bolder;}</style>
+<style>body {background-image: url("../img/biblioteca.jpg");font-weight: bolder;}</style>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="../index.php">Menu</a>
+  <a class="navbar-brand" href="../index.php"><img src="../img/booklogo.png" style="width: 6vw;"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
