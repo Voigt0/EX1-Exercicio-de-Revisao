b3e4e6042b51450044a698c1464da0843a60cbae
Voigt0 <voigtrodrigo0@gmail.com>
2022-03-08 08:04:00 -0300

    Update tabelaeditora.php

--- php/tabelaeditora.php
+++ php/tabelaeditora.php
@@ -20,7 +20,7 @@
 </head>
 <body class="">
     <?php include_once "menu.php"; ?>
-    <form method="post">
+    <form method="post" style="background-color: rgb(33, 37, 41); color: #FFF;">
         <input type="radio" id="EDI_ID" name="busca" value="EDI_ID" <?php if($busca == "EDI_ID"){echo "checked";}?>>
         <label for="huey"><h3>#ID</h3></label>
         <br>
@@ -50,7 +50,6 @@
                     <th scope="col">Fundação</th>
                     <th scope="col">Fundador</th>
                     <th scope="col">Sede</th>
-                    <th scope="row">DETALHE</th>
                     <th scope="row">ALTERAR</th>
                     <th scope="row">EXCLUIR</th>
                 </tr>
@@ -78,7 +77,6 @@
                     <td><?php echo date("d/m/Y", strtotime($linha['EDI_FUNDACAO']));?></td>
                     <td><?php echo $linha['EDI_FUNDADOR'];?></td>
                     <td><?php echo $linha['EDI_SEDE'];?></td>
-                    <td scope="row"><svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16"><path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/></svg></td>
                     <td scope="row"><a href="cadeditora.php?id=<?php echo $linha['EDI_ID'];?>&comando=update"><img src="../img/history-solid.svg" style="width: 2rem;"></a></td>
                     <td><a onclick="return confirm('Deseja mesmo excluir?')" href="acao.php?id=<?php echo $linha['EDI_ID'];?>&tabela=editora&comando=deletar"><img src="../img/trash.svg" style="width: 2rem;"></a></td>
                 </tr>
