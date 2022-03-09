0e46a671df2ac3f2e7114adc96aeb3d40a97f287
Voigt0 <voigtrodrigo0@gmail.com>
2022-03-08 22:52:12 -0300

    Update index.php

--- index.php
+++ index.php
@@ -22,72 +22,87 @@
 </head>
 <body class="">
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="../index.php"><img src="../img/booklogo.png" style="width: 6vw;"></a>
+  <a class="navbar-brand" href="../index.php"><img src="img/booklogo.png" style="width: 6vw;"></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   
-  <a class="navbar-brand" href="tabelacliente.php">CLIENTE</a>
+  <a class="navbar-brand" href="php/tabelacliente.php">CLIENTE</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   
-  <a class="navbar-brand" href="tabelaeditora.php">EDITORA</a>
+  <a class="navbar-brand" href="php/tabelaeditora.php">EDITORA</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   
-  <a class="navbar-brand" href="tabelagenero.php">GÊNERO</a>
+  <a class="navbar-brand" href="php/tabelagenero.php">GÊNERO</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <a class="navbar-brand" href="tabelaendereco.php">ENDEREÇO</a>
+  <a class="navbar-brand" href="php/tabelaendereco.php">ENDEREÇO</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <a class="navbar-brand" href="tabelatitulo.php">TÍTULO</a>
+  <a class="navbar-brand" href="php/tabelatitulo.php">TÍTULO</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <a class="navbar-brand" href="tabelaexemplar.php">EXEMPLAR</a>
+  <a class="navbar-brand" href="php/tabelaexemplar.php">EXEMPLAR</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <a class="navbar-brand" href="tabelatitgen.php">TIT_GEN</a>
+  <a class="navbar-brand" href="php/tabelatitgen.php">TIT_GEN</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <a class="navbar-brand" href="tabelaemprestimo.php">EMPRÉSTIMO</a>
+  <a class="navbar-brand" href="php/tabelaemprestimo.php">EMPRÉSTIMO</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <a class="navbar-brand" href="cadendereco.php">INSERIR ENDEREÇO</a>
+  <a class="navbar-brand" href="php/cademprestimo.php">INSERIR EMPRÉSTIMO</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <a class="navbar-brand" href="cadtitulo.php">INSERIR TÍTULO</a>
+  <a class="navbar-brand" href="php/cadexemplar.php">INSERIR EXEMPLAR</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <a class="navbar-brand" href="php/cadtitgen.php">INSERIR TIT_GEN</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <a class="navbar-brand" href="php/cadendereco.php">INSERIR ENDEREÇO</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <a class="navbar-brand" href="php/cadtitulo.php">INSERIR TÍTULO</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   
-  <a class="navbar-brand" href="cadcliente.php">INSERIR CLIENTE</a>
+  <a class="navbar-brand" href="php/cadcliente.php">INSERIR CLIENTE</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   
-  <a class="navbar-brand" href="cadeditora.php">INSERIR EDITORA</a>
+  <a class="navbar-brand" href="php/cadeditora.php">INSERIR EDITORA</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   
-  <a class="navbar-brand" href="cadgenero.php">INSERIR GÊNERO</a>
+  <a class="navbar-brand" href="php/cadgenero.php">INSERIR GÊNERO</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
