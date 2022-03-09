16837cf075fde956e631e0477a49990d73f29299
Voigt0 <voigtrodrigo0@gmail.com>
2022-03-08 22:52:21 -0300

    Update cadexemplar.php

--- php/cadexemplar.php
+++ php/cadexemplar.php
@@ -38,6 +38,7 @@
     </header>
     <content>
         <form action="acao.php" method="post" id="form">
+            <p class="formItem formText" id="formNome">Título:</p>
             <select name="TIT_ID" value="">
             <?php
                 $pdo = Conexao::getInstance();
