fa9dad5c86ad6a076d88ce4d2de6c6ee15fc014e
Voigt0 <voigtrodrigo0@gmail.com>
2022-03-10 22:20:44 -0300

    Update index.php

--- index.php
+++ index.php
@@ -72,7 +72,10 @@
     <span class="navbar-toggler-icon"></span>
   </button>
 
-  <a class="navbar-brand" href="php/cadexemplar.php">INSERIR EXEMPLAR</a>
+</nav>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+
+<a class="navbar-brand" href="php/cadexemplar.php">INSERIR EXEMPLAR</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
