54643c567f2cb44b630174dcea038d0334a52ea8
Voigt0 <voigtrodrigo0@gmail.com>
2022-03-11 22:44:56 -0300

    Update cadtitulo.php

--- php/cadtitulo.php
+++ php/cadtitulo.php
@@ -24,6 +24,7 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Biblioteca</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel='stylesheet' type='text/css' media='screen' href='../css/cadastro.css'>
     <script src='../js/main.js'></script>
     <link rel="icon" type="image/x-icon" href="../img/favicon.ico">
@@ -58,7 +59,7 @@
             <input type="date" class="formItem formInput" name="TIT_LANCAMENTO" id="TIT_LANCAMENTO" value="<?php if ($comando == "update"){echo $dados['TIT_LANCAMENTO'];}?>">
             <br><br>
             <p class="formItem formText" id="formNome">Editora:</p>
-            <select name="EDI_ID" value="">
+            <select class="form-select" name="EDI_ID" value="">
             <?php
                 $pdo = Conexao::getInstance();
                 $consulta = $pdo->query("SELECT * FROM editora");
